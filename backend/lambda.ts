54da90086c177057938c9ef3420c2901479084bf
KoniPN <kong.18th@gmail.com>
2025-10-29 17:28:00 +0700

    fix import not use in ec2

--- backend/lambda.ts
+++ backend/lambda.ts
@@ -1,5 +1,5 @@
-import serverlessExpress from "@vendia/serverless-express";
-import express from "./server.js";
+// import serverlessExpress from "@vendia/serverless-express";
+// import express from "./server.js";
 
-// Export Lambda handler
-export const handler = serverlessExpress({ app: express });
+// // Export Lambda handler
+// export const handler = serverlessExpress({ app: express });
