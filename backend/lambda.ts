db8f12ffdae8761002b183e8571002cc80dbd137
KoniPN <kong.18th@gmail.com>
2025-10-29 11:50:26 +0700

    add file terraform

---
+++ backend/lambda.ts
@@ -0,0 +1,5 @@
+import serverlessExpress from "@vendia/serverless-express";
+import express from "./server.js";
+
+// Export Lambda handler
+export const handler = serverlessExpress({ app: express });
