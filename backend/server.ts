fe77e3979f8002065c017472a5a455080a46596b
KoniPN <kong.18th@gmail.com>
2025-10-31 17:47:29 +0700

    change url

--- backend/server.ts
+++ backend/server.ts
@@ -28,7 +28,7 @@ console.log(BASE_URL);
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://shortsun.online",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
